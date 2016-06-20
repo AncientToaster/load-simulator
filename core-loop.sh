a94d3f78a5800a72c78e93bc4f39cc543eec2ff9
Austin Schaefer <austin@newrelic.com>
2016-06-20 16:51:51 -0700

    call network script in core loop
    Signed-off-by: Austin Schaefer <austin@newrelic.com>

--- core-loop.sh
+++ core-loop.sh
@@ -7,8 +7,8 @@ export master_log="$base_directory/logs/master.log"
 export verbose_log="$base_directory/logs/verbose.log"
 export disk_log="$base_directory/logs/disk.log"
 export cpu_log="$base_directory/logs/cpu.log"
-#export network_log="$base_directory/logs/network.log"
-#export network_files_log="$base_directory/logs/network_files.log"
+export network_log="$base_directory/logs/network.log"
+export network_files_log="$base_directory/logs/network_files.log"
 #export ram_log="$base_directory/logs/ram.log"
 
 # Randomly assign an ID for this particular run
@@ -17,6 +17,7 @@ echo -e "$(date -u) > Started: Script run $script_run_id" | tee -a $master_log $
 
 # Call both stress tests and run them at the same time
 /bin/bash $base_directory/disk.sh > /dev/null &
+/bin/bash $base_directory/network.sh > /dev/null &
 /bin/bash $base_directory/cpu.sh > /dev/null
 echo -e "$(date -u) > Finished: Script run $script_run_id" | tee -a $master_log $verbose_log
 
