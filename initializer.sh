a98bf6f8ea086169687fcac932f7cc774abd93c2
Austin Schaefer <austin@newrelic.com>
2016-06-20 20:52:44 -0700

    allow user to supply max iterations
    Signed-off-by: Austin Schaefer <austin@newrelic.com>

--- initializer.sh
+++ initializer.sh
@@ -3,11 +3,35 @@
 # Get the base directory for the app
 export base_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# `starts a new copy of the performance script every eight minutes
-while :
-do
-    echo "Starting a new run, press [CTRL+C] to stop..."
-    echo -e "$(date -u) PID: $$ $BASHPID"
-    /bin/bash $base_directory/core-loop.sh > /dev/null &
-    sleep 8m
-done
+# If no run count supplied, run forever
+if [ -z "$1" ]
+then
+    echo "No run count supplied, running indefinitely"
+    echo "To supply run count, invoke as: bash initializer.sh ###"
+    # Runs forever because this always evaluates to true
+    while :
+        do
+            echo "Starting a new run, press [CTRL+C] to stop..."
+            echo -e "$(date -u) PID: $$ $BASHPID"
+            Invokes script and sends STDOUT from script to null
+            /bin/bash "$base_directory"/core-loop.sh > /dev/null &
+            # Simultaneously starts 8 minute timer before invoking script again
+            sleep 8m
+        done
+# If run count supplied, run that many times
+else
+    # Store the maximum number of runs
+    script_iterations=$1
+    # Initialize run_count_start
+    run_count_start=1
+    echo "Running $script_iterations times"
+    while [[ $run_count_start -le $script_iterations ]]
+    do
+        echo "Running $run_count_start of $script_iterations iterations"
+        echo -e "$(date -u) PID: $$ $BASHPID"
+        /bin/bash "$base_directory"/core-loop.sh > /dev/null &
+        sleep 8m
+        let ++run_count_start
+    done
+    echo "Completed all $script_iterations iterations"
+fi
--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 ## Installation ##
 
-Load simulator is a simple bash script. It has a few dependencies:
+The load simulator scripts have a few dependencies:
 
-+ **stress-ng**: To install on Ubuntu, run `sudo apt-get install stress-ng`
++ **wget**: To fetch remote network resources
+    + To install on Ubuntu, run `sudo apt-get install wget`
+    + To install on CentOS 7, run `sudo yum install wget`
++ **stress-ng**: Generates the CPU stress
+    + To install on Ubuntu, run `sudo apt-get install stress-ng`
+    + To install on CentOS 7, run `wget ftp://ftp.icm.edu.pl/vol/rzm5/linux-opensuse/repositories/server:/monitoring/SLE_11_SP4/x86_64/stress-ng-0.06.02-1.1.x86_64.rpm && sudo rpm -Uvh stress-ng-0.06.02-1.1.x86_64.rpm`
 
 ## Invoking the script ##
 
@@ -10,9 +15,11 @@ To run forever, start `initializer.sh` with no arguments:
 
     bash initializer.sh
 
-To run N times, start `initializer.sh` with N arguments:
+To run N times, start `initializer.sh` and specify N as the first argument:
 
-    ./initializer
+    bash initializer.sh N
+    bash initializer.sh 3
+    bash initializer.sh 1000
 
 ## Program structure ##
 
