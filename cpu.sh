7fa61cce5fb5dd9d628d65619b2b8966a9ea886e
Austin Schaefer <austin@newrelic.com>
2016-06-23 13:08:25 -0700

    sub-job IDs for CPU jobs
    Signed-off-by: Austin Schaefer <austin@newrelic.com>

--- cpu.sh
+++ cpu.sh
@@ -8,11 +8,13 @@ function cpuStress {
     # Log time and load for CPUJ1
     stress_time=$(shuf -i 480-720 -n 1)
     stress_cpu_load=$(shuf -i 10-30 -n 1)
-    echo -e "$(date -u) >>> CPUJ1: Running for $stress_time seconds at $stress_cpu_load load" | tee -a $cpu_log $verbose_log
+    cpuj1_sub_id=$(shuf -i 1-1000000000 -n 1)
+    echo -e "$(date -u) >>> CPUJ1/"$cpuj1_sub_id" running for $stress_time seconds at $stress_cpu_load load" | tee -a $cpu_log $verbose_log
     # Log time and load for CPUJ2
     stress_timer_freq=$(shuf -i 1000-50000 -n 1)
     stress_timer_ops=$(shuf -i 3600000-7200000 -n 1)
-    echo -e "$(date -u) >>> CPUJ2: Running for $stress_timer_ops ops at $stress_timer_freq load" | tee -a $cpu_log $verbose_log
+    cpuj2_sub_id=$(shuf -i 1-1000000000 -n 1)
+    echo -e "$(date -u) >>> CPUJ2/"$cpuj2_sub_id" running for $stress_timer_ops ops at $stress_timer_freq load" | tee -a $cpu_log $verbose_log
     # Run the test at the specified load and frequency
     stress-ng --cpu 1 --cpu-load $stress_cpu_load -t $stress_time --timer 1 --timer-freq $stress_timer_freq --timer-ops $stress_timer_ops
     echo -e "$(date -u) >> Finished: CPU run $cpu_run_id" | tee -a $cpu_log $master_log $verbose_log
