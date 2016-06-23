2cb47fdc648fcb632145c339cc2bb0546ed98856
Austin Schaefer <austin@newrelic.com>
2016-06-23 13:02:34 -0700

    fix syntax errors in ram.sh
    Signed-off-by: Austin Schaefer <austin@newrelic.com>

--- ram.sh
+++ ram.sh
@@ -13,7 +13,7 @@ function ramJob {
     # Log the ramjob run time and load amount
     echo -e "$(date -u) >>> RJ$ram_base_id/$ram_sub_id occupying $ram_load_amount MB for $ram_run_time seconds" | tee -a $ram_log $verbose_log
     # Occupy $ram_load_amount MB for #ram_run_time seconds
-    stress-ng --vm 1 --vm-bytes $ram_load_amount MB --vm-hang $ram_run_time -t $ram_run_time
+    stress-ng --vm 1 --vm-bytes "$ram_load_amount"M --vm-hang $ram_run_time -t $ram_run_time
     echo -e "$(date -u) >>> RJ$ram_base_id/$ram_sub_id finished, freeing $ram_load_amount MB" | tee -a $ram_log $verbose_log
 }
 
@@ -26,7 +26,7 @@ function ramStress {
     # Occupy 100 to 200 MB of ram for 360 to 480 seconds
     ramJob 1 $(shuf -i 100-200 -n 1) $(shuf -i 360-480 -n 1) &
     # Occupy 25 to 75 MB of ram for 60 to 480 seconds
-    ramJob 2 $(shuf -i 100-200 -n 1) $(shuf -i 360-480 -n 1)
+    ramJob 2 $(shuf -i 25-75 -n 1) $(shuf -i 360-480 -n 1)
     echo -e "$(date -u) >> Finished: RAM run $ram_run_id" | tee -a $ram_log $master_log $verbose_log
 }
 
